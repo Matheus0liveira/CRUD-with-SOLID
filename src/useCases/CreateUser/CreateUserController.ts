a343fe05ee9971dd1689d54c20c7884919ea1146
Matheus0liveira <matheusfilho98.ms@gmail.com>
2021-04-16 09:54:59 -0300

    feat: create CreatUserController

---
+++ src/useCases/CreateUser/CreateUserController.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import CreateUserCase from './CreateUserUseCase';
+
+export default class CreateUserController {
+
+  constructor(
+    private createUserCase: CreateUserCase,
+  ){};
+
+  async handle(request: Request, response: Response): Promise<Response> {
+
+    const { name, email, password} = request.body;
+
+    try{
+      await this.createUserCase.execute({name, email, password});
+
+      return response.send(201).send();
+      
+    }catch(err){
+      return response.status(400).json({
+        message: err.message || 'Unexpected error.'
+      });
+    };
+
+  };
+};
