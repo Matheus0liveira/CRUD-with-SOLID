f9fe0e365ab24a9ff9850a95acfa97da6eeeb945
Matheus Oliveira <matheus0liveira@pop-os.localdomain>
2021-04-16 09:31:43 -0300

    feat: create mailProvider

--- src/useCases/CreateUser/CreateUserUseCase.ts
+++ src/useCases/CreateUser/CreateUserUseCase.ts
@@ -1,10 +1,12 @@
 import { User } from '../../entities/User';
+import { IMailProvider } from '../../providers/IMailProvider';
 import { IUserRepository } from '../../repositories/IUsersRepository';
 import { ICreateUserRequestDTO } from './CreateUserDTO';
 
 export default class CreateUserCase {
   constructor(
-    private usersRepository: IUserRepository
+    private usersRepository: IUserRepository,
+    private mailProvider: IMailProvider,
   ){};
 
   async execute(data: ICreateUserRequestDTO){
@@ -18,6 +20,19 @@ export default class CreateUserCase {
     const user = new User(data);
 
     await this.usersRepository.save(user);
+
+    this.mailProvider.sendEmail({
+      to: {
+        name: data.name,
+        email: data.email,
+      },
+      from: {
+        name: 'Equipe App',
+        email: 'equipe@mail.com',
+      },
+      subject: 'Seja bem vindo',
+      body: '<p> Seja bem-vindo ao nosso APP.</p>'
+    })
   };
 
 
